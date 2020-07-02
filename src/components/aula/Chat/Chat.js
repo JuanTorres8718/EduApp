d6f08079128eee4f7f2ebb31fe988e1fa7bf195d
JuanTorres8718 <jdtorres8718@gmail.com>
2020-07-02 18:07:52 -0500

    se agrego chat

--- src/components/aula/Chat/Chat.js
+++ src/components/aula/Chat/Chat.js
@@ -1,35 +1,36 @@
-import React from "react";
-import Nav from "../componentAula/NavAula";
-import Mensajes from "./Mensajes";
-import MensajeOtro from "./MensajeOtro";
-import "../../../style/Chat.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import React from 'react'
+import Nav from '../componentAula/NavAula'
+import Mensajes from './Mensajes'
+import MensajeOtro from './MensajeOtro'
+import '../../../style/Chat.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 const Chat = () => {
-  return (
-    <div className="">
-      <Nav className="col-lg-4"></Nav>
-      <div id="chat">
-        <Mensajes mensaje="me ayudan con el ejercicio 5"></Mensajes>
-        <Mensajes mensaje="¿hoy hay clase?" />
-        <MensajeOtro mensaje="si, hay clase a las 5" />
-        
-          <div className="fixed-bottom p-3 border-top d-flex justify-content-between bg-dark "
-          id="boton">
-            <input
-              type="text"
-              placeholder="escribe aqui tu mensaje"
-              className="rounded col-lg-8"
-            ></input>
-            <button type="submit" className="boton">
-              <FontAwesomeIcon icon={faPaperPlane} />
-            </button>
-         
-        </div>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className=''>
+			<Nav className='col-lg-4'></Nav>
+			<div id='chat'>
+				<Mensajes mensaje='me ayudan con el ejercicio 5'></Mensajes>
+				<Mensajes mensaje='¿hoy hay clase?' />
+				<MensajeOtro mensaje='si, hay clase a las 5' />
 
-export default Chat;
+				<div
+					className='fixed-bottom p-3 border-top d-flex justify-content-between bg-dark '
+					id='boton'
+				>
+					<input
+						type='text'
+						placeholder='escribe aqui tu mensaje'
+						className='rounded col-lg-8'
+					></input>
+					<button type='submit' className='boton'>
+						<FontAwesomeIcon icon={faPaperPlane} />
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Chat
