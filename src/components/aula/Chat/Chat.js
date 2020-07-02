60111d6017e61cb1831de10b357b2cc710caa875
haflorez29 <heidyflorez55@gmail.com>
2020-07-02 12:21:55 -0500

    arreglo boton

--- src/components/aula/Chat/Chat.js
+++ src/components/aula/Chat/Chat.js
@@ -3,6 +3,8 @@ import Nav from "../componentAula/NavAula";
 import Mensajes from "./Mensajes";
 import MensajeOtro from "./MensajeOtro";
 import "../../../style/Chat.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 const Chat = () => {
   return (
@@ -12,15 +14,18 @@ const Chat = () => {
         <Mensajes mensaje="me ayudan con el ejercicio 5"></Mensajes>
         <Mensajes mensaje="¿hoy hay clase?" />
         <MensajeOtro mensaje="si, hay clase a las 5" />
-        <div className="fixed-bottom p-3 border-top d-flex justify-content-between bg-dark">
-          <input
-            type="text"
-            placeholder="escribe aqui tu mensaje"
-            className="rounded col-lg-8"
-          ></input>
-          <button type="submit" className="boton">
-            enviar
-          </button>
+        
+          <div className="fixed-bottom p-3 border-top d-flex justify-content-between bg-dark "
+          id="boton">
+            <input
+              type="text"
+              placeholder="escribe aqui tu mensaje"
+              className="rounded col-lg-8"
+            ></input>
+            <button type="submit" className="boton">
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+         
         </div>
       </div>
     </div>
